d1bf97d3a5abc19cc046d71841b7ab188dea6b39
rum <nhatrum98@gmail.com>
2018-10-09 15:01:42 +0700

    Finish english

---
+++ src/rum/dev/English.java
@@ -0,0 +1,28 @@
+package rum.dev;
+
+import java.util.Scanner;
+
+public class English {
+	
+	public static void main(String args[]){
+	      
+		 char n = '4';
+	      switch(n)
+	      {
+	         case '0' : System.out.println("zero!"); break;
+	         case '1' : System.out.println("one!"); break;
+	         case '2' : System.out.println("two!"); break;
+	         case '3' : System.out.println("three!"); break;
+	         case '4' : System.out.println("four!"); break;
+	         case '5' : System.out.println("five!"); break;
+	         case '6' : System.out.println("six!"); break;
+	         case '7' : System.out.println("seven!"); break;
+	         case '8' : System.out.println("eight!"); break;
+	         case '9' : System.out.println("nine!"); break;
+
+	         default :
+	            System.out.println("khong co");
+	      }
+	      System.out.println("Tu tieng anh nam o vi tri " + n);
+	   }
+	}
